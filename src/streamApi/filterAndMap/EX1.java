6ddfd2c0106d425b6c1f4789e4ce0f7621153be6
govindkrcrimfo <govind.krcrimfo555@gmail.com>
2025-12-04 23:13:50 +0530

    1.Convert list of String to Uppercase 2.filter string that starts with a specific character

--- src/streamApi/filterAndMap/EX1.java
+++ src/streamApi/filterAndMap/EX1.java
@@ -1,16 +1,31 @@
 package streamApi.filterAndMap;
+
 import java.util.*;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
-/*
- * Given a list of integers, return only even numbers.
-*/
 public class EX1 {
     public static void main(String[] args) {
-        List<Integer> l=Arrays.asList(1,2,3,4,5,6,7,8);
-        List<Integer> evenNumbers=l.stream().filter(x->x%2==0).collect(Collectors.toList());
-        System.out.print("Even numbers "+ evenNumbers );
+
+        /*
+         * Given a list of integers, return only even numbers.
+         */
+
+        List<Integer> l = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> evenNumbers = l.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.print("Even numbers " + evenNumbers);
         System.out.println();
-    } 
+
+        /*
+         * 1.Convert list of strings to uppercase
+         * 2.Filter strings that start with a specific character
+         */
+
+        List<String> ls = Arrays.asList("Govind ", "kumar", "Gupta", "Howrah", "gopal");
+        List<String> upperLs=ls.stream().map(s->s.toUpperCase()).toList();
+        List<String > strtwithG=ls.stream().filter(s->s.toUpperCase().startsWith("G")).toList();
+        System.out.println("Upper case  string : "+upperLs);
+        System.out.println();
+        System.out.println("String starts with - "+strtwithG);
+
+    }
 }
